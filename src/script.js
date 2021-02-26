1f3be2ef874e06b83d262527a1bde0eadadd3b81
Sara-pixie <78561671+Sara-pixie@users.noreply.github.com>
2021-02-26 17:42:55 +0100

    input buttons JS and fixed Equal button error

--- src/script.js
+++ src/script.js
@@ -4,13 +4,20 @@ function clearAll(event){
 }
 function calculate(event){
     let calculated = eval(formula.innerHTML);
-    formula.innerHTML += `=${calculated}`;
-    output.innerHTML = calculated;
+    if (calculated === undefined){
+        clearAll();
+    } else{
+        formula.innerHTML += `=${calculated}`;
+        output.innerHTML = calculated;
+    }
 }
-function inputValue(input){
+function handleInput(event){
+    event.preventDefault();
+    let input = this.innerHTML;
     formula.innerHTML += input;
     output.innerHTML = input;
 }
+
 let formula = document.querySelector("#formula");
 let output = document.querySelector("#output");
 formula.innerHTML = null;
@@ -18,3 +25,9 @@ output.innerHTML = null;
 
 let isEqual = document.querySelector("#isEqual").addEventListener("click", calculate);
 let clear = document.querySelector("#clear").addEventListener("click", clearAll);
+
+let inputButtons =["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "decimal", "add", "subtract", "multiply", "divide"];
+inputButtons.forEach(function(button){
+    let buttonId = `#${button}`;
+    document.querySelector(buttonId).addEventListener("click", handleInput);
+});
